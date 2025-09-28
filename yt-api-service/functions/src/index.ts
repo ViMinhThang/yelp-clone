0ab9f4f93de379090f6b509d04b3a84f3b2013ef
ViMinhThang <huynhminhthang246@gmail.com>
2025-09-28 17:14:44 +0700

    "coding upload video function"

--- yt-api-service/functions/src/index.ts
+++ yt-api-service/functions/src/index.ts
@@ -1,32 +1,51 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * import {onCall} from "firebase-functions/v2/https";
- * import {onDocumentWritten} from "firebase-functions/v2/firestore";
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
-
-import {setGlobalOptions} from "firebase-functions";
-import {onRequest} from "firebase-functions/https";
 import * as logger from "firebase-functions/logger";
+import * as functionsV1 from "firebase-functions/v1";
+import {initializeApp} from "firebase-admin/app";
+import {getFirestore} from "firebase-admin/firestore";
+import {Storage} from "@google-cloud/storage";
+import {onCall} from "firebase-functions/v2/https";
+initializeApp();
+const firestore = getFirestore("yt-clone-cfa09");
+const storage = new Storage();
+const rawVideoBucketName = "v-fragile-yt-raw-videos";
 
-// Start writing functions
-// https://firebase.google.com/docs/functions/typescript
+export const createUser = functionsV1
+  .region("asia-southeast1")
+  .auth.user()
+  .onCreate(async (user) => {
+    const userInfo = {
+      uid: user.uid,
+      email: user.email,
+      photoUrl: user.photoURL,
+    };
 
-// For cost control, you can set the maximum number of containers that can be
-// running at the same time. This helps mitigate the impact of unexpected
-// traffic spikes by instead downgrading performance. This limit is a
-// per-function limit. You can override the limit for each function using the
-// `maxInstances` option in the function's options, e.g.
-// `onRequest({ maxInstances: 5 }, (req, res) => { ... })`.
-// NOTE: setGlobalOptions does not apply to functions using the v1 API. V1
-// functions should each use functions.runWith({ maxInstances: 10 }) instead.
-// In the v1 API, each function can only serve one request per container, so
-// this will be the maximum concurrent request count.
-setGlobalOptions({ maxInstances: 10 });
+    try {
+      await firestore.collection("users").doc(user.uid).set(userInfo);
+      logger.info(`User Created: ${JSON.stringify(userInfo)}`);
+    } catch (err) {
+      logger.error("Error writing user to Firestore", err);
+    }
+  });
+export const generateUploadUrl = onCall(
+  {maxInstances: 1, region: "asia-southeast1"},
+  async (request) => {
+    if (!request.auth) {
+      throw new functionsV1.https.HttpsError(
+        "failed-precondition",
+        "The function must be called while authenticated"
+      );
+    }
+    const auth = request.auth;
+    const data = request.data;
+    const bucket = storage.bucket(rawVideoBucketName);
 
-export const helloWorld = onRequest((request, response) => {
-  logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
-});
+    const fileName = `${auth.uid}-${Date.now()}.${data.fileExtension}`;
+
+    const [url] = await bucket.file(fileName).getSignedUrl({
+      version: "v4",
+      action: "write",
+      expires: Date.now() + 15 * 60 * 1000,
+    });
+    return {url, fileName};
+  }
+);
